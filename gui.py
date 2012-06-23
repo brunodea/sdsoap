74586666042397d579e5950698b1387922095c8d
Vitor da Silva <vitords.42@gmail.com>
2012-06-23 20:28:29 -0300

    gui atualizada, metodos funcionando

--- gui.py
+++ gui.py
@@ -1,12 +1,42 @@
 
-
+import sys
+from PyQt4.QtCore import * #QAbstractListModel, QModelIndex
 from PyQt4.QtGui import QMainWindow
 from gui_mainwindow import Ui_MainWindow
 
 class GUI(QMainWindow, Ui_MainWindow):
+
     def __init__(self):
 		# Inicia a janela principal
         QMainWindow.__init__(self)
         # Inicia a UI criada no Qt Designer
         self.setupUi(self)
 		
+    def setGameName(self, gameName):
+        self.gameName.setText(gameName)
+		
+    def setGamePrice(self, gamePrice):
+        self.gamePrice.setText(gamePrice)
+		
+    def setGameInfo(self, gameInfo):
+        self.gameInfo.setText(gameInfo)
+        
+    def setGameList(self, gameList):
+        lm = ListModel(gameList, self)
+        self.gameList.setModel(lm)
+        
+class ListModel(QAbstractListModel): 
+    def __init__(self, datain, parent=None, *args): 
+        """ datain: a list where each item is a row
+        """
+        QAbstractListModel.__init__(self, parent, *args) 
+        self.listdata = datain
+ 
+    def rowCount(self, parent=QModelIndex()): 
+        return len(self.listdata) 
+ 
+    def data(self, index, role): 
+        if index.isValid() and role == Qt.DisplayRole:
+            return QVariant(self.listdata[index.row()])
+        else: 
+            return QVariant()
--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 
 import sys
-from gui import GUI
 from PyQt4.QtGui import QApplication
+from gui import GUI
 
 def main():
-	# Inicia o fluxo de controle do Qt
-	app = QApplication(sys.argv)
-	# Instancia uma GUI
-	gui = GUI()
-	# Mostra a janela
-	gui.show()
-	# Finaliza a execucao do Qt quando a janela e fechada
-	sys.exit(app.exec_())
+    # Inicia o fluxo de controle do Qt
+    app = QApplication(sys.argv)
+    # Instancia uma GUI
+    gui = GUI()
+    # Mostra a janela
+    list = ['Hello Kitty Online', 'Barbie Dress-Up', 'Teletubbies']
+    gui.setGameList(list)
+    gui.show()
+    # Finaliza a execucao do Qt quando a janela e fechada
+    sys.exit(app.exec_())
 
 if __name__ == '__main__':
     main()
