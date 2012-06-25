d2926ce54910b9eb55a6d4944a8a06ab4f774495
Bruno Azevedo <brunodea@inf.ufsm.br>
2012-06-25 16:02:14 -0300

    Dei uma melhorada no código.
    ATENÇÃO! Só tenho 5000 requests pra fazer no bing por mês. Nesse momento só tenho mais 4982 transações, então cuidado pra não esgotar tudo até a apresentação.

--- search.py
+++ search.py
@@ -6,17 +6,19 @@ sys.path.append('../')
 import logging
 import traceback as tb
 import suds.metrics as metrics
-import httplib
 import inspect
 import urllib
+import urllib2
+import httplib
+import suds
 from suds import WebFault
 from suds.client import Client
-from suds.plugin import MessagePlugin
-from HTMLParser import HTMLParser
+import urllib
+import contextlib
+from xml.dom.minidom import parseString
 
-
-MS_URL = 'http://api.bing.net/search.wsdl'
-MS_KEY = 'PsC095DMrg/uXAzzXZxJUTUo5Iuov92f2RbW/7fazlc='
+MS_KEY = 'mjKwRazoJLnNhZyPnuBXr7u3lj/BCbj2CB4BFIzY4Rg='
+MS_URL = 'https://api.datamarket.azure.com/Bing/Search/Image'
 
 class Game(object):
     def __init__(self,name='',platform='',price=0,gameid=''):
@@ -29,27 +31,8 @@ class Game(object):
     def __str__(self):
         return 'Jogo: %s\nPlataforma: %s\nPreco: %d\nID: %s\n' % (self.name,self.platform,self.price,self.gameid)
 
-class MyHTMLParser(HTMLParser):
-    def handle_starttag(self,tag,attrs):
-        try:
-            len(self.imgs)
-        except Exception as e:
-            self.imgs = []
-
-        if tag == "img":
-            for attr, value in attrs:
-                if attr == "src":
-                    self.imgs.append(value)
-                    print value
-                    
-    def handl_endtag(self,tag):
-        pass
-    def handle_data(self,data):
-        pass
-
-
 def setup_logging():
-    logging.basicConfig(level=loggin.INFO)
+    logging.basicConfig(level=logging.INFO)
     logging.getLogger('suds.client').setLevel(logging.DEBUG)
 
 
@@ -59,48 +42,6 @@ def getGamePlatform(display_string):
     res = res[::-1]
 
     return res
-
-class MyPlugin(MessagePlugin):
-    def marshalled(self,context):
-        print context.envelope
-
-def getGameImages(gamename, gameplatform='', extravalues='', num=3):
-    result = []
-
-    try:
-        print 'Busca por imagem do jogo.'
-        client = Client(MS_URL,plugins=[MyPlugin()])
-        print 'Iniciando busca...'
-        print client
-#        arrayofsourcetype = client.factory.create('ArrayOfSourceType')
-#        sourcetype = client.factory.create('SourceType')
-#        arrayofsourcetype.SourceType = [sourcetype.Image]
-
-#        imagerequest = client.factory.create('ImageRequest')
-#        imagerequest.Offset = 0
-#        imagerequest.Count = num
-
-#        searchrequest = client.factory.create('SearchRequest')
-#        searchrequest.parameters.AppId = MS_KEY
-#        searchrequest.parameters.Query = gamename + ' ' + gameplatform + ' ' + extravalues
-#        searchrequest.parameters.Image = imagerequest
-#        searchrequest.parameters.Sources = arrayofsourcetype
-
-#        response =  client.service.Search(searchrequest)
-
-#        print 'Resultados da busca...'
-#        print response
-    except WebFault as f:
-        print 'WebFault'
-        print f
-        print f.fault
-    except Exception as e:
-        print 'Exception'
-        print e
-        tb.print_exc()
-
- 
-    return result
 
 def getGamesFromSearch(search):
     result = []
@@ -113,28 +54,45 @@ def getGamesFromSearch(search):
         print 'Resultados da busca:'
         responses = response.colResults.SearchResult
 
-        httpServ = httplib.HTTPConnection('www.google.com',80)
-        httpServ.connect()
         for r in responses:
             name = r.SelectString
             platform = getGamePlatform(r.DisplayString)
             gameid = r.ItemID
 
             game = Game(name=name,platform=platform,gameid=gameid)
-            print game
-            query = name + '-' + platform
-            query = urllib.urlencode({'q' : query})
-            httpServ.request('GET','/search?sugexp=chrome,mod%3D5&'+query+'&um=1&ie=UTF-8&hl=pt-BR&tbm=isch&source=og&sa=N&tab=wi&authuser=0&ei=J6rmT5O4O42c8gS8r_WiAQ&biw=1215&bih=688&sei=KarmT9nXIq636QGO5IjgDg')
-#            getGameImages(gamename=name,gameplatform=platform)
-            response = httpServ.getresponse()
-            if response.status == httplib.OK:
-                parser = MyHTMLParser()
-                parser.feed(response.read())
-            else:
-                print "ERROOOO"
+            #print game
 
             result.append(game)
-        httpServ.close()
+            
+        passman = urllib2.HTTPPasswordMgrWithDefaultRealm()
+        passman.add_password(None, MS_URL,' ',MS_KEY)
+        
+        authhandler = urllib2.HTTPBasicAuthHandler(passman)
+        opener = urllib2.build_opener(authhandler)
+        
+        urllib2.install_opener(opener)
+        return_stmt = []
+        for r in result:
+            print '-----------------------Get Image Request-------------------------'
+           
+            query = '\''+r.name + ' ' + r.platform +'\''
+            query = urllib.urlencode({'Query':query})
+            url = MS_URL+'?'+query
+            with contextlib.closing(urllib2.urlopen(url)) as x:
+                data = x.read()
+                dom = parseString(data)
+                xmltag = dom.getElementsByTagName('d:MediaUrl')
+                gameimages = []
+                for elem in xmltag:
+                    xml = elem.toxml()
+                    imageurl = xml.replace('<d:MediaUrl m:type="Edm.String">','').replace('</d:MediaUrl>','')
+                    if '.png' in imageurl:
+                        gameimages.append(imageurl)
+                        print imageurl
+                    if len(gameimages) == 5:
+                        break
+                r.imgs = gameimages
+                return_stmt.append(r)               
 
     except WebFault as f:
         print 'WebFault'
@@ -143,14 +101,12 @@ def getGamesFromSearch(search):
     except Exception as e:
         print 'Exception'
         print e
-#        tb.print_exc()
 
-    return result
+    return return_stmt
 
 def main():
+    setup_logging()
     getGamesFromSearch('megaman')
-#    for game in getGamesFromSearch('megaman'):
-#        print game
 
 
 
