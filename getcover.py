9220ac153cbb564083b8eac4ba5e29b72367bded
davi russi <davirussi@gmail.com>
2012-06-26 21:56:08 -0300

    minor changes in the function getcover

--- getcover.py
+++ getcover.py
@@ -1,15 +1,18 @@
 import urllib
 import re
 
-#id is provided by the web service
-id='1859458'
-#build de url
-url='http://www.gamecompare.com/product.aspx?productid='+id
-print url
-#open the link
-f = urllib.urlopen(url)
-#get url of the cover
-urlcover = re.search('<div class="productimage"><img src=\"(.*?)\" alt=', f.read()).group(1)
-#downlaod de cover
-print 'Downloading: ' + urlcover
-urllib.urlretrieve(urlcover, "00000002.jpg")
+def downloadcover (id,covername):
+    #id is provided by the web service
+    id='1859458'
+    #build de url
+    url='http://www.gamecompare.com/product.aspx?productid='+id
+    #print url
+    #open the link
+    f = urllib.urlopen(url)
+    #get url of the cover
+    urlcover = re.search('<div class="productimage"><img src=\"(.*?)\" alt=', f.read()).group(1)
+    #downlaod de cover
+    print 'Downloading: ' + urlcover
+    urllib.urlretrieve(urlcover, covername+".jpg")    
+    
+downloadcover('1859458','covertemp')
